8d74ed66ac51bf14e77e946e3bbc8ab67523e955
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2020-11-23 15:01:29 +0100

    aggiunto firebase crash analitycs e corretto splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
 import 'root/navigator_tablet.dart';
-import 'services/navigation_service.dart';
+// import 'services/navigation_service.dart';
+// import 'package:flutter/foundation.dart' show kDebugMode;
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +32,15 @@ void main() async {
   final shared = new Shared();
   final themeString = await shared.getThemeMode() ?? Constants.themeLight;
   final theme = themeString == Constants.themeLight ? appTheme : appThemeDark;
+
+  // if (kDebugMode) {
+  //   // Force disable Crashlytics collection while doing every day development.
+  //   // Temporarily toggle this to true if you want to test crash reporting in your app.
+  //   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  // } else {
+  //   // Handle Crashlytics enabled status when not in Debug,
+  //   // e.g. allow your users to opt-in to crash reporting.
+  // }
 
   setupLocator();
   runApp(MyApp(theme: theme, themeName: themeString));
